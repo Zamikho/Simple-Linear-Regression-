2e24c033e4e47cb6f0623fcd39a0ed7f64c2f269
Zamikho <subecerra@ucdavis.edu>
2018-08-16 00:00:09 -0700

    Split the data set into training and test set

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -1,4 +1,4 @@
-# Import the libraries 
+#Import the libraries 
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -7,4 +7,8 @@ import pandas as pd
 dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc([:, :-1].values
 y = dataset.iloc([:, 1].values
+
+# Split the datasrt into Training and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y test_size = 1/3, random_state = 0)
   
