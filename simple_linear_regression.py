bff5a7b11b622fbd61d22b88fced813ee015288c
Zamikho <subecerra@ucdavis.edu>
2018-08-19 21:57:40 -0700

    finish model

--- simple_linear_regression.py
+++ simple_linear_regression.py
@@ -1,14 +1,28 @@
-#Import the libraries 
+#Import the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Import the data set
+# Import the Data Set
 dataset = pd.read_csv('Salary_Data.csv')
-X = dataset.iloc([:, :-1].values
-y = dataset.iloc([:, 1].values
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 1].values
 
-# Split the datasrt into Training and Test set
+# Split the Data Set into Training and Test Set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y test_size = 1/3, random_state = 0)
-  
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+# Fit Simple Linear Regression to the Training Set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the Test Set Results
+y_pred = regressor.predict(X_test)
+
+# Ask User for Years of Experience
+exp = input("Enter Years of Experience: ")
+exp = int(exp)
+salary = regressor.predict(exp)
+salary = int(salary)
+print("With", exp, "years of experience, you should obtain a salary of approximately $", salary)
